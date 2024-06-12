df08238c4b8a4b87f82c2a5af23b186e4babe0e8
Lekan <jimohridwanlekan@gmail.com>
2024-06-12 21:05:02 +0100

    Add accounts GET API

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
+import { QueryProvider } from "@/providers/query-provider";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -17,10 +18,11 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+      <html lang="en">
+        <body className={inter.className}>
+          <QueryProvider>{children}</QueryProvider>
+        </body>
+      </html>
     </ClerkProvider>
   );
 }
