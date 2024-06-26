e88f909172f6cdf045e9c70bee31319fc481417c
Lekan <jimohridwanlekan@gmail.com>
2024-06-26 16:47:34 +0100

    Add accounts POST API

--- db/schema.ts
+++ db/schema.ts
@@ -1,7 +1,10 @@
+import { createInsertSchema } from "drizzle-zod";
 import { pgTable, text } from "drizzle-orm/pg-core";
 
 export const accounts = pgTable("accounts", {
   id: text("id").primaryKey(),
   name: text("name").notNull(),
   userId: text("user_id").notNull(),
-})
+});
+
+export const insertAccountSchema = createInsertSchema(accounts)
